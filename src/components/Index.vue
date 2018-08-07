aa1bdad1e4e13d0833d4c5f38bbe308c637335aa
ycx <ycx@example.com>
2018-08-07 23:01:24 +0800

    VUE

---
+++ src/components/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <img v-bind:src="img" alt="">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "index",
+    data() {
+      return {
+        msg: '这是首页',
+        img: 'static/img/logo.png'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
